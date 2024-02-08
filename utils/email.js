939448562c96bf5e28d6f4f2e494609fb0286048
taner <tanerozer16@gmail.com>
2024-02-08 15:57:46 +0300

    email js added

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,30 @@
 const nodemailer = require('nodemailer');
 
-// Function to send email using Nodemailer
 const sendEmail = async (options) => {
-  // 1) Create a transporter
+  // 1) Transportör oluştur
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
+    // Özel e-posta sunucusu yapılandırması için gerekli çevresel değişkenleri sağlayın
+    // host: process.env.EMAIL_HOST,
+    // port: process.env.EMAIL_PORT,
+    // secure: true, // true for 465, false for other ports
+    // requireTLS: true,
   });
 
-  // 2) Define the email options
+  // 2) E-posta seçeneklerini tanımla
   const mailOptions = {
-    from: 'Jonas Schmedtmann <hello@jonas.io>', // Sender name and email
-    to: options.email, // Receiver email address
-    subject: options.subject, // Email subject
-    text: options.message, // Email message body
-    // html: // If you want to send HTML content, uncomment this line and provide HTML content
+    from: `Taner Özer <${process.env.EMAIL_FROM}>`, // Gönderen bilgisi
+    to: options.email, // Alıcı e-posta adresi
+    subject: options.subject, // E-posta konusu
+    text: options.message, // Metin tabanlı e-posta içeriği
+    // html: options.html, // HTML e-posta içeriği için
   };
 
-  // 3) Actually send the email
+  // 3) E-postayı gönder
   await transporter.sendMail(mailOptions);
 };
 
