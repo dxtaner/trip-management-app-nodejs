aa7f9b029f1c28e94089d17ad3f3976d54b82197
taner <tanerozer16@gmail.com>
2024-02-08 13:57:15 +0300

    user routes edited

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,36 @@
-// Importing required modules
-const express = require("express");
-const userController = require("./../controllers/userController.js");
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController.js');
 
-// Creating a router instance
 const router = express.Router();
 
-// Routes handling CRUD operations for users
+// Authentication routes
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Update password route (protected)
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+
+// Update and delete current user route (protected)
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+// User routes
 router
-  .route("/")
-  .get(userController.getAllUsers) // Route for getting all users
-  .post(userController.createUser); // Route for creating a new user
+  .route('/')
+  .get(userController.getAllUsers) // Get all users
+  .post(userController.createUser); // Create a new user
 
 router
-  .route("/:id")
-  .get(userController.getUser) // Route for getting a specific user
-  .patch(userController.updateUser) // Route for updating a specific user
-  .delete(userController.deleteUser); // Route for deleting a specific user
+  .route('/:id')
+  .get(userController.getUser) // Get user by ID
+  .patch(userController.updateUser) // Update user by ID
+  .delete(userController.deleteUser); // Delete user by ID
 
-// Exporting the router module
 module.exports = router;
