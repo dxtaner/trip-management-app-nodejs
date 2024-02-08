b0e6e9719a4e60e88bd7138bbaa7a5242b62538e
taner <tanerozer16@gmail.com>
2024-02-08 13:58:01 +0300

    tour routes edited

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,29 +1,35 @@
-// Importing required modules
 const express = require('express');
-const tourController = require('./../controllers/tourController.js');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
-// Creating a router instance
 const router = express.Router();
 
-router.get(
-  '/top-5-cheap',
-  tourController.aliasTopTours,
-  tourController.getAllTours
+// Route for getting the top 5 cheap tours
+router.route('/top-5-cheap').get(
+  tourController.aliasTopTours, // Middleware to alias top tours
+  tourController.getAllTours // Handler to get all tours
 );
-router.get('/tour-stats', tourController.getTourStats);
-router.get('/monthly-plan/:year', tourController.getMonthlyPlan);
 
-// Routes handling CRUD operations for tours
+// Route for getting tour statistics
+router.route('/tour-stats').get(tourController.getTourStats);
+
+// Route for getting monthly plan for a year
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+// Routes for CRUD operations on tours
 router
   .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(authController.protect, tourController.getAllTours) // Protected route to get all tours
+  .post(tourController.createTour); // Route to create a new tour
 
 router
   .route('/:id')
-  .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .get(tourController.getTour) // Route to get a tour by ID
+  .patch(tourController.updateTour) // Route to update a tour by ID
+  .delete(
+    authController.protect, // Protected route
+    authController.restrictTo('admin', 'lead-guide'), // Restricted route based on user role
+    tourController.deleteTour // Route to delete a tour by ID
+  );
 
-// Exporting the router module
 module.exports = router;
