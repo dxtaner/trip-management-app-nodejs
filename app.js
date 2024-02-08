5ca0bb91be88b89fae291da859a5cde0ffe6bf6b
taner <tanerozer16@gmail.com>
2024-02-08 13:56:48 +0300

    edited app js

--- app.js
+++ app.js
@@ -1,36 +1,69 @@
 // Importing required modules
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 // Importing route modules
-const tourRouter = require('./routes/tourRoutes.js');
-const userRouter = require('./routes/userRoutes.js');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+// Importing custom middleware and error handling modules
+const { errorLogger } = require('./utils/logger');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 // Creating an Express application
 const app = express();
 
-const { errorLogger } = require('./utils/logger');
-const AppError = require('./utils/appError.js');
-const globalErrorHandler = require('./controllers/errorController.js');
-
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
 // Development logging using Morgan middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 // Middleware for handling errors
 app.use((err, req, res, next) => {
   errorLogger.error(`An error occurred: ${err.message}`);
   res.status(500).send('Something went wrong!');
 });
-
-// Parsing incoming JSON data middleware
-app.use(express.json());
-
-// Serving static files from the public directory
-app.use(express.static(`${__dirname}/public`));
 
 // Custom middleware logging a message to the console
 app.use((req, res, next) => {
@@ -44,16 +77,18 @@ app.use((req, res, next) => {
   next();
 });
 
-// 3) ROUTES
+// 2) ROUTES
 
 // Mounting route handlers
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
 
+// Handling undefined routes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// Global error handling middleware
 app.use(globalErrorHandler);
 
 // Exporting the configured Express application
