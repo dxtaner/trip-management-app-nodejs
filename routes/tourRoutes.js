6ef324ed1427b8c304412ffed8013f1278a56276
taner <tanerozer16@gmail.com>
2024-02-09 14:07:37 +0300

    tourroutes edited

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,35 +1,60 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
-// Route for getting the top 5 cheap tours
-router.route('/top-5-cheap').get(
-  tourController.aliasTopTours, // Middleware to alias top tours
-  tourController.getAllTours // Handler to get all tours
-);
+// Mounting the review routes
+router.use('/:tourId/reviews', reviewRouter);
+
+// Route for getting the top 5 cheapest tours
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 // Route for getting tour statistics
 router.route('/tour-stats').get(tourController.getTourStats);
 
-// Route for getting monthly plan for a year
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+// Route for getting monthly plan
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
 
-// Routes for CRUD operations on tours
+// Route for getting tours within a certain distance from a point
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+// Route for getting distances from a point to all tours
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
+// CRUD operations for tours
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours) // Protected route to get all tours
-  .post(tourController.createTour); // Route to create a new tour
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 
 router
   .route('/:id')
-  .get(tourController.getTour) // Route to get a tour by ID
-  .patch(tourController.updateTour) // Route to update a tour by ID
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
   .delete(
-    authController.protect, // Protected route
-    authController.restrictTo('admin', 'lead-guide'), // Restricted route based on user role
-    tourController.deleteTour // Route to delete a tour by ID
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
   );
 
 module.exports = router;
