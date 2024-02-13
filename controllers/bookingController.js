33826424b55d457b66d1b785114c99c31a5786d9
taner <tanerozer16@gmail.com>
2024-02-13 14:34:30 +0300

    booking controller added

---
+++ controllers/bookingController.js
@@ -0,0 +1,53 @@
+const dotenv = require('dotenv');
+const conf = dotenv.config({ path: './config.env' });
+
+const stripe = require('stripe')(conf.STRIPE_SECRET_KEY);
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel.js');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+// Get checkout session for a tour
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        amount: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
+
+// Create booking checkout (temporary, insecure)
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+// CRUD operations for bookings
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
