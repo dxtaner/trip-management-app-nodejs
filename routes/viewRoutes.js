64512822dde58b16d0cf0dbdf02d1b00c5d8b400
taner <tanerozer16@gmail.com>
2024-02-14 15:13:51 +0300

    alerts, getGuidePage, getAboutPage and getSingupForm added

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,19 +2,18 @@ const express = require('express');
 const router = express.Router();
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
+
+router.use(viewsController.alerts);
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/signup', authController.isLoggedIn, viewsController.getSingupForm);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/about', viewsController.getAboutPage);
+router.get('/become-a-guide', viewsController.getGuidePage);
 
-router.get(
-  '/my-tours',
-  bookingController.createBookingCheckout,
-  authController.protect,
-  viewsController.getMyTours,
-);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
   '/submit-user-data',
