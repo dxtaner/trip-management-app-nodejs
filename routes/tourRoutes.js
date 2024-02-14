ff4b9605aa6e5fbf08a5217c1da06d722d966e53
taner <tanerozer16@gmail.com>
2024-02-14 14:42:51 +0300

    uploadTourImage added resizeTourImages added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,42 +5,35 @@ const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
-// Mounting the review routes
 router.use('/:tourId/reviews', reviewRouter);
 
-// Route for getting the top 5 cheapest tours
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-// Route for getting tour statistics
 router.route('/tour-stats').get(tourController.getTourStats);
 
-// Route for getting monthly plan
 router
   .route('/monthly-plan/:year')
   .get(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide', 'guide'),
-    tourController.getMonthlyPlan
+    tourController.getMonthlyPlan,
   );
 
-// Route for getting tours within a certain distance from a point
 router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
 
-// Route for getting distances from a point to all tours
 router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
 
-// CRUD operations for tours
 router
   .route('/')
   .get(tourController.getAllTours)
   .post(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
-    tourController.createTour
+    tourController.createTour,
   );
 
 router
@@ -49,12 +42,14 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
-    tourController.updateTour
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
+    tourController.updateTour,
   )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
-    tourController.deleteTour
+    tourController.deleteTour,
   );
 
 module.exports = router;
