a740c8f77020b9fb54ca6c1f9db1a124b3e70eb9
taner <tanerozer16@gmail.com>
2024-02-14 14:26:25 +0300

    getSingupForm, getAboutPage, getGuidePage added

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,6 +4,14 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking')
+    res.locals.alert =
+      "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up here immediatly, please come back later.";
+  next();
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
   res.status(200).render('overview', {
@@ -27,6 +35,24 @@ exports.getTour = catchAsync(async (req, res, next) => {
     tour,
   });
 });
+
+exports.getSingupForm = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Create your account!',
+  });
+};
+
+exports.getAboutPage = (req, res) => {
+  res.status(200).render('about', {
+    title: 'about!',
+  });
+};
+
+exports.getGuidePage = (req, res) => {
+  res.status(200).render('become-a-guide', {
+    title: 'become-a-guide!',
+  });
+};
 
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
