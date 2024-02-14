c615852306c27fc5374edaa6f2ce06115077d82f
taner <tanerozer16@gmail.com>
2024-02-14 14:25:15 +0300

    edited index js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const conf = dotenv.config({ path: './config.env' });
 // Database connection setup
 require('./db_connection/database.js');
 
-const port = process.env.PORT || 3033;
+const port = conf.parsed?.PORT || 4500;
 
 // Start the Express application listening on the specified port
 const server = app.listen(port, () => {
   combinedLogger.info(
-    `Server is running on http://localhost:${server.address().port}`
+    `Server is running on http://localhost:${server.address().port}`,
   );
 });
