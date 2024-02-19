e7a43bd5b9917dbc2642cf57defb90c2a30f485a
taner <tanerozer16@gmail.com>
2024-02-19 15:45:59 +0300

    edited project

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,39 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
   tour: {
     type: mongoose.Schema.ObjectId,
-    ref: 'Tour',
-    required: [true, 'Booking must belong to a Tour!']
+    ref: "Tour",
+    required: [true, "Booking must belong to a Tour!"],
   },
   user: {
     type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Booking must belong to a User!']
+    ref: "User",
+    required: [true, "Booking must belong to a User!"],
   },
   price: {
     type: Number,
-    required: [true, 'Booking must have a price.']
+    required: [true, "Booking must have a price."],
   },
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
   },
   paid: {
     type: Boolean,
-    default: true
-  }
+    default: true,
+  },
 });
 
 // Populate user and tour data before any find query
-bookingSchema.pre(/^find/, function(next) {
-  this.populate('user').populate({
-    path: 'tour',
-    select: 'name'
+bookingSchema.pre(/^find/, function (next) {
+  this.populate("user").populate({
+    path: "tour",
+    select: "name",
   });
   next();
 });
 
-const Booking = mongoose.model('Booking', bookingSchema);
+const Booking = mongoose.model("Booking", bookingSchema);
 
 module.exports = Booking;
