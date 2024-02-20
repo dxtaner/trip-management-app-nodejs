de957bf7fc25568d644cb41308f438f4775bfa28
taner <tanerozer16@gmail.com>
2024-02-20 15:52:14 +0300

    updateSettings js added

---
+++ public/js/updateSettings.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import axios from "axios";
+import { showAlert } from "./alert";
+
+// type is either 'password' or data
+export const updateSettings = async (data, type) => {
+  try {
+    const url = type === "password" ? "api/users/updateMyPassword" : "api/users/updateme";
+    const res = await axios({
+      method: "PATCH",
+      url: url,
+      data: data,
+    });
+    if (res.data.status === "success") {
+      showAlert("success", `${type.toUpperCase()} updated successfully!`);
+      location.reload();
+    }
+  } catch (err) {
+    showAlert("error", err.response.data.message);
+  }
+};
